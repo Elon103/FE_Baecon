c5cd37ba60c6c899287b4b3e826419f38fe05f64
Elon103 <minhson031094@gmail.com>
2022-03-09 16:48:40 +0700

    Make function show map point

--- main.js
+++ main.js
@@ -7,6 +7,8 @@ searchButton.addEventListener('click', () => {
     getUserName(inputValue);
 
 });
+var MapPoints = [];
+//chrome
 // Get info by user_name
 function getUserName(inputValue) {
     $.ajax({
@@ -20,13 +22,25 @@ function getUserName(inputValue) {
         success: function(data) {
             var v_data = data.result;
             $("#tb_employee").find('tr').remove();
-            $("#card_name").text("Lịch sử di chuyển của nhân viên " + data.result[0].user_name);
+            // console.log(v_data);
+            if (v_data !== null) {
+                $("#card_name").text("Lịch sử di chuyển của nhân viên " + data.result[0].user_name);
+            }
             v_data.forEach((element, index) => {
-                buildBody(element, index + 1);
+                var v_value = { "lat": element.latitude, "lng": element.longitude, "time": dateTime(new Date(element.time)) };
+                // console.log(v_value);
+                MapPoints.push(v_value);
+                if (index < 15) {
+                    buildBody(element, index + 1);
+                }
             });
+            // console.log((JSON.stringify(MapPoints)));
+            google.maps.event.addDomListener(window, 'load', initialize(JSON.stringify(MapPoints)));
         }
     });
 }
+
+
 //Build grid body
 function buildBody(data, i) {
     $("#tb_employee").append(
@@ -50,11 +64,61 @@ function buildBody(data, i) {
 
 function dateTime(date) {
     var year = date.getFullYear();
-    var month = date.getMonth();
+    // var month = date.getMonth();
     var day = date.getDate();
     var hours = date.getHours();
     var minutes = date.getMinutes();
     var second = date.getSeconds();
-    var text = day + "/" + month + "/" + year + " : " + hours + ":" + minutes + ":" + second;
+    var text = day + "/" + 2 + "/" + year + " : " + hours + ":" + minutes + ":" + second;
     return text;
+}
+
+// 
+
+var MY_MAPTYPE_ID = 'custom_style';
+
+function initialize(MapPoints) {
+
+    if (jQuery('#map').length > 0) {
+
+        var locations = jQuery.parseJSON(MapPoints);
+
+        window.map = new google.maps.Map(document.getElementById('map'), {
+            mapTypeId: google.maps.MapTypeId.ROADMAP,
+            scrollwheel: false
+        });
+
+        var infowindow = new google.maps.InfoWindow();
+        var flightPlanCoordinates = [];
+        var bounds = new google.maps.LatLngBounds();
+
+        for (i = 0; i < locations.length; i++) {
+            marker = new google.maps.Marker({
+                position: new google.maps.LatLng(locations[i].lat, locations[i].lng),
+                map: map
+            });
+            flightPlanCoordinates.push(marker.getPosition());
+            bounds.extend(marker.position);
+
+            //open popup when click marker
+
+            google.maps.event.addListener(marker, 'click', (function(marker, i) {
+                return function() {
+                    infowindow.setContent(locations[i]['time']);
+                    infowindow.open(map, marker);
+                }
+            })(marker, i));
+        }
+
+        map.fitBounds(bounds);
+
+        var flightPath = new google.maps.Polyline({
+            map: map,
+            path: flightPlanCoordinates,
+            strokeColor: "#FF0000",
+            strokeOpacity: 1.0,
+            strokeWeight: 2
+        });
+
+    }
 }
